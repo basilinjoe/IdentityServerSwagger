809fed2d91c46eab615ee7f3052d1a841cd3bb47
Basilin Joe <basilinjoe@gmail.com>
2019-11-17 17:37:38 +0530

    initial commit

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                new ApiResource("api", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client {
+                    ClientId = "api.swagger",
+                    ClientName = "Swagger UI for api",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+                    RedirectUris = {"https://localhost:44300/oauth2-redirect.html"},
+                    AllowedScopes = {"api"}
+                }
+            };
+    }
+}
